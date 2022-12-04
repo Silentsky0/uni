8a6f9d441b938afd451a64758ee6c2b4d73d348c
Paweł Cichowski <cichowski.pawel1@gmail.com>
2022-12-04 12:38:21 +0100

    some cleanup

--- sbd/src/main.c
+++ sbd/src/main.c
@@ -7,17 +7,7 @@
 #include <string.h>
 #include "stdlib.h"
 
-int hashchuj(const char* str, int h)
-{
-    // One-byte-at-a-time hash based on Murmur's mix
-    // Source: https://github.com/aappleby/smhasher/blob/master/src/Hashes.cpp
-    for (; *str; ++str) {
-        h ^= *str;
-        h *= 0x5bd1e995;
-        h ^= h >> 15;
-    }
-    return h;
-}
+
 
 int main (int argc, char *argv[]) {
 
@@ -29,7 +19,6 @@ int main (int argc, char *argv[]) {
     if (argc >= 2) {
         for (int i = 0; i < argc; i++) {
             if (strcmp(argv[i], "--help") == 0 || strcmp(argv[i], "-h") == 0) {
-                printf("");
 
                 return 0;
             }
@@ -53,13 +42,9 @@ int main (int argc, char *argv[]) {
             if (strcmp(argv[i], "--file-path") == 0 || strcmp(argv[i], "-f") == 0) {
 
             }
-
         }
     }
 
-
-
-    //disk_generate_random("data/generated.file", requested_records);
     disk_generate_records("data/generated.file", requested_records);
 
     sort_distribution_phase("data/generated.file", print_contents);
@@ -67,6 +52,6 @@ int main (int argc, char *argv[]) {
     sort_sorting_phase(print_contents, print_every_phase);
 
     sort_postprocess_phase(requested_records, 0);
-
-    return 0;
 }
+
+
